5a9142036ba94fdf08aa38944e2b2bf1a89a60bd
Mathias Balogh <mathiasbalogh@Mathias-MacBook-Air.local>
2016-12-17 16:01:33 -0600

    modify arrow image and animation onclick

--- script.js
+++ script.js
@@ -3,5 +3,15 @@ jQuery(document).ready(function(){
   $(".duties").hide();
   $("button").on("click", function(){
    $(".duties").slideToggle();
+   if($('#arrow').css('height') === '40px'){
+     $('#arrow').css('height', '275px');
+   }else{
+     $('#arrow').css('height', '40px');
+   }
+   if($('#arrow').css('height') === '275px'){
+     $('#arrow').css('background-position','-7px 0');
+   }else if ($('#arrow').css('height') === '40px'){
+     $('#arrow').css('background-position', '-7px -225px');
+   }
  });
 });
