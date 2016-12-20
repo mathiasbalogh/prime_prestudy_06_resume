86f56d85ac2be3e9b47d8e16009b50563a10c609
Mathias Balogh <mathiasbalogh@Mathias-MacBook-Air.local>
2016-12-19 22:39:16 -0600

    add button and sprite functionality

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
 
-jQuery(document).ready(function(){
+$(document).ready(function(){
   $(".duties").hide();
-  $("button").on("click", function(){
-   $(".duties").slideToggle();
-   if($('#arrow').css('height') === '40px'){
-     $('#arrow').css('height', '275px');
-   }else{
-     $('#arrow').css('height', '40px');
-   }
-   if($('#arrow').css('height') === '275px'){
-     $('#arrow').css('background-position','-7px 0');
-   }else if ($('#arrow').css('height') === '40px'){
-     $('#arrow').css('background-position', '-7px -225px');
-   }
- });
+  $('button').on('click', function(){
+    $(this).closest("section").find(".duties").slideToggle();
+    $(this).toggleClass("arrow");
+  });
 });
